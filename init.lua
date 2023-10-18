4d3bd503b87e9fe0070a663e7911197b117e741b
Daniel Toint <Gedztodu@gmail.com>
2023-10-18 17:10:16 +0800

    ADd relative number and number

--- init.lua
+++ init.lua
@@ -19,9 +19,10 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
 	{'preservim/nerdtree'},
-  {'folke/tokyonight.nvim'},
     {'williamboman/mason.nvim'},
   {'williamboman/mason-lspconfig.nvim'},
+    { 'rose-pine/neovim', name = 'rose-pine' },
+
 
   -- LSP Support
   {
@@ -69,7 +70,6 @@ require "nvim-treesitter.configs".setup {
 }
 
 vim.opt.termguicolors = true
-vim.cmd.colorscheme('tokyonight')
 
 
 local lsp_zero = require('lsp-zero')
@@ -119,3 +119,63 @@ cmp.setup({
 
 
 
+require('rose-pine').setup({
+	--- @usage 'auto'|'main'|'moon'|'dawn'
+	variant = 'main',
+	--- @usage 'main'|'moon'|'dawn'
+	dark_variant = 'main',
+	bold_vert_split = false,
+	dim_nc_background = false,
+	disable_background = false,
+	disable_float_background = false,
+	disable_italics = false,
+
+	--- @usage string hex value or named color from rosepinetheme.com/palette
+	groups = {
+		background = 'base',
+		background_nc = '_experimental_nc',
+		panel = 'surface',
+		panel_nc = 'base',
+		border = 'highlight_med',
+		comment = 'muted',
+		link = 'iris',
+		punctuation = 'subtle',
+
+		error = 'love',
+		hint = 'iris',
+		info = 'foam',
+		warn = 'gold',
+
+		headings = {
+			h1 = 'iris',
+			h2 = 'foam',
+			h3 = 'rose',
+			h4 = 'gold',
+			h5 = 'pine',
+			h6 = 'foam',
+		}
+		-- or set all headings at once
+		-- headings = 'subtle'
+	},
+
+	-- Change specific vim highlight groups
+	-- https://github.com/rose-pine/neovim/wiki/Recipes
+	highlight_groups = {
+		ColorColumn = { bg = 'rose' },
+
+		-- Blend colours against the "base" background
+		CursorLine = { bg = 'foam', blend = 10 },
+		StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+
+		-- By default each group adds to the existing config.
+		-- If you only want to set what is written in this config exactly,
+		-- you can set the inherit option:
+		Search = { bg = 'gold', inherit = false },
+	}
+})
+
+-- Set colorscheme after options
+vim.cmd('colorscheme rose-pine')
+
+vim.opt.relativenumber = true
+vim.opt.number = true
